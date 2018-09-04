d964b813c229a447d28a1a6d7c68e377fb47699c
suvi shahoyi <sumaya.shahoyi@gmail.com>
2018-09-04 07:13:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
-// Code your solution in this file!
-function logDriverNames(drivers){
- drivers.forEach(function (driver,index,array){
-   console.log(driver.name)
- })
+function countdown(callback) {
+  setTimeout(function () {
+    callback();
+  }, 2000);
 }
+
+function createMultiplier(multiplyValue) {
+  return function (value) {
+    return multiplyValue * value;
+  };
+}
+
+function multiplier(a, b) {
+  return a * b;
+}
+
+var doubler = createMultiplier(2);
+var tripler = createMultiplier(3);
+var doublerWithBind = multiplier.bind(null, 2);
+var triplerWithBind = multiplier.bind(null, 3);
